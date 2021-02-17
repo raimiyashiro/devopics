5165cbdba9d4ff0c383da78411b8d7b1ca7989bf
raimiyashiro <dev.raimiyashiro@gmail.com>
2021-02-16 23:23:36 -0300

    project setup

---
+++ src/main/java/com/rgmiyashiro/devopics/DevopicsApplication.java
@@ -0,0 +1,13 @@
+package com.rgmiyashiro.devopics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rgmiyashiro/devopics/DevopicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rgmiyashiro.devopics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
