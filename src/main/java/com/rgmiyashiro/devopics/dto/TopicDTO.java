b521ca0866ad8e6b4f10804ec9e2f8a428fe05dd
Raí Miyashiro <52873303+raimiyashiro@users.noreply.github.com>
2021-02-17 23:08:10 -0300

    Update TopicDTO.java

--- src/main/java/com/rgmiyashiro/devopics/dto/TopicDTO.java
+++ src/main/java/com/rgmiyashiro/devopics/dto/TopicDTO.java
@@ -20,7 +20,7 @@ public class TopicDTO {
 
         Set<Tag> tagEntities = this.collectTags(tags);
 
-        return new Topic(title, url, tagEntities);
+        return new Topic(this.title, this.url, this.tagEntities);
     }
 
     public Set<Tag> collectTags(Set<TagDTO> tags) {
