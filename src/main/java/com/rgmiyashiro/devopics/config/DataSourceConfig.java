1b4c2504058a29c415ed613840d7c2db7ba81c38
raimiyashiro <dev.raimiyashiro@gmail.com>
2021-02-23 09:58:02 -0300

    fix: addes system.getenv

--- src/main/java/com/rgmiyashiro/devopics/config/DataSourceConfig.java
+++ src/main/java/com/rgmiyashiro/devopics/config/DataSourceConfig.java
@@ -1,6 +1,5 @@
 package com.rgmiyashiro.devopics.config;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,13 +13,12 @@ public class DataSourceConfig {
     @Bean
     public DataSource getDataSource() {
 
-        Dotenv dotenv = Dotenv.load();
 
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName(dotenv.get("devopics_db_driver"));
-        dataSourceBuilder.url(dotenv.get("devopics_db_url"));
-        dataSourceBuilder.username(dotenv.get("devopics_db_username"));
-        dataSourceBuilder.password(dotenv.get("devopics_db_password"));
+        dataSourceBuilder.driverClassName(System.getenv("devopics_db_driver"));
+        dataSourceBuilder.url(System.getenv("devopics_db_url"));
+        dataSourceBuilder.username(System.getenv("devopics_db_username"));
+        dataSourceBuilder.password(System.getenv("devopics_db_password"));
         return dataSourceBuilder.build();
     }
 }
