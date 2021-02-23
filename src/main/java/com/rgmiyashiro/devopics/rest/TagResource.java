6f430bd1dde83c06c0cb72fb2aec6aa7e78b23ca
raimiyashiro <dev.raimiyashiro@gmail.com>
2021-02-23 17:33:01 -0300

    security: added cors policy

--- src/main/java/com/rgmiyashiro/devopics/rest/TagResource.java
+++ src/main/java/com/rgmiyashiro/devopics/rest/TagResource.java
@@ -3,12 +3,14 @@ package com.rgmiyashiro.devopics.rest;
 import com.rgmiyashiro.devopics.model.Tag;
 import com.rgmiyashiro.devopics.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://devopics.herokuapp.com/")
 public class TagResource {
 
     @Autowired
