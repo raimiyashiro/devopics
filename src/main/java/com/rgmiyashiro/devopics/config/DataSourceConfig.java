3006f0ab429b3cb190f3a41d7b41fd8edf20f93f
raimiyashiro <dev.raimiyashiro@gmail.com>
2021-02-24 11:45:18 -0300

    cors: added client origin

--- src/main/java/com/rgmiyashiro/devopics/config/DataSourceConfig.java
+++ src/main/java/com/rgmiyashiro/devopics/config/DataSourceConfig.java
@@ -1,5 +1,6 @@
 package com.rgmiyashiro.devopics.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +13,21 @@ public class DataSourceConfig {
 
     @Bean
     public DataSource getDataSource() {
+        Dotenv dotenv = Dotenv.load();
 
 
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+
         dataSourceBuilder.driverClassName(System.getenv("devopics_db_driver"));
         dataSourceBuilder.url(System.getenv("devopics_db_url"));
         dataSourceBuilder.username(System.getenv("devopics_db_username"));
         dataSourceBuilder.password(System.getenv("devopics_db_password"));
+
+
+//        dataSourceBuilder.driverClassName(dotenv.get("devopics_db_driver"));
+//        dataSourceBuilder.url(dotenv.get("devopics_db_url"));
+//        dataSourceBuilder.username(dotenv.get("devopics_db_username"));
+//        dataSourceBuilder.password(dotenv.get("devopics_db_password"));
         return dataSourceBuilder.build();
     }
 }
--- src/main/java/com/rgmiyashiro/devopics/rest/TagResource.java
+++ src/main/java/com/rgmiyashiro/devopics/rest/TagResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://devopics.herokuapp.com/")
+@CrossOrigin(origins = {"https://devopics.herokuapp.com"})
 public class TagResource {
 
     @Autowired
