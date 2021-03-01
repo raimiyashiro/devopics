4ab78e62483b9b2916c5a9c4438e493d8b6c1eb6
raimiyashiro <dev.raimiyashiro@gmail.com>
2021-03-01 09:35:18 -0300

    removed cross origin from classes

--- src/main/java/com/rgmiyashiro/devopics/rest/TopicResource.java
+++ src/main/java/com/rgmiyashiro/devopics/rest/TopicResource.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://devopics.herokuapp.com/**")
 @RestController
 public class TopicResource {
 
