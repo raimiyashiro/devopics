e56fff7595a047d58d09faff436f8d4faa795546
raimiyashiro <dev.raimiyashiro@gmail.com>
2021-03-01 09:21:57 -0300

    fix: removed @CrossOrigin

--- src/main/java/com/rgmiyashiro/devopics/rest/TagResource.java
+++ src/main/java/com/rgmiyashiro/devopics/rest/TagResource.java
@@ -3,7 +3,6 @@ package com.rgmiyashiro.devopics.rest;
 import com.rgmiyashiro.devopics.model.Tag;
 import com.rgmiyashiro.devopics.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin
 public class TagResource {
 
     @Autowired
