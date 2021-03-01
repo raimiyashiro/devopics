cbae70d4a7bd68e43938ce76a2226bfd63d19eef
raimiyashiro <dev.raimiyashiro@gmail.com>
2021-03-01 11:16:45 -0300

    i literally HATE cors

--- src/main/java/com/rgmiyashiro/devopics/rest/TagResource.java
+++ src/main/java/com/rgmiyashiro/devopics/rest/TagResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://devopics.herokuapp.com/**")
+@CrossOrigin
 @RestController
 public class TagResource {
 
--- src/main/java/com/rgmiyashiro/devopics/rest/TopicResource.java
+++ src/main/java/com/rgmiyashiro/devopics/rest/TopicResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://devopics.herokuapp.com/**")
+@CrossOrigin
 @RestController
 public class TopicResource {
 
