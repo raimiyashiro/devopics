1bc6810d31a52ed1192a53aa9e1d659ee08d3d70
raimiyashiro <dev.raimiyashiro@gmail.com>
2021-03-01 10:08:34 -0300

    test: omg

--- src/main/java/com/rgmiyashiro/devopics/DevopicsApplication.java
+++ src/main/java/com/rgmiyashiro/devopics/DevopicsApplication.java
@@ -2,12 +2,27 @@ package com.rgmiyashiro.devopics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DevopicsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopicsApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+						.addMapping("/**")
+						.allowedOrigins("https://devopics.herokuapp.com");
+			}
+		};
 	}
 
 }
